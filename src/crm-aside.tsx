a5f3eed26bc3a89e458dd48029a1f20fa192b797
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-09-06 12:49:08 -0300

    teste

--- src/crm-aside.tsx
+++ src/crm-aside.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import singleSpaReact from 'single-spa-react'
 import Root from './root.component'
 
+const teste = 'teste'
+
 const lifecycles = singleSpaReact({
 	React,
 	ReactDOM,
